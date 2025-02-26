73296bc3a3b1836092e419e27d8e27a14868528e
Aman Bhogal <aman.bhogal@innovixx.co.uk>
2025-02-26 09:57:48 +0000

    chore: update eslint config to v3.0.7

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,10 @@
 import baseConfig from '@innovixx/eslint-config/config/configs/base/index.mjs';
 import typescriptConfig from '@innovixx/eslint-config/config/configs/typescript/index.mjs';
 
-export const defaultESLintIgnores = [
-  '**/.*',
-  '**/.git',
-  '**/README.md',
-  '**/dist/',
-  '**/build/',
-  '**/node_modules/',
-  '**/temp/',
-];
-
 export default [
   baseConfig,
   typescriptConfig,
   {
-    ignores: [
-      ...defaultESLintIgnores,
-    ],
-  },
-  {
     files: ['**/*.{js,jsx,ts,tsx}'],
   },
-  {
-    rules: {
-      'import/extensions': 'off',
-      'import/no-unresolved': 'off',
-      'no-undef': 'off',
-    },
-  }];
+];
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/require-await
 const main = async (): Promise<void> => {
   // eslint-disable-next-line no-console
   console.log('Start');
